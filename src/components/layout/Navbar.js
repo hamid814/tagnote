9cf2f6a32fd717c01b7fe655d9d0d2e1a16bb4ea
hamid <hamid331994@gmail.com>
2020-03-27 21:22:18 +0430

    dev

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
       <div className='navbar-right'>
         <ThemeButton />
         <button className='insert-button' onClick={onInsert}>
-          insert
+          Insert
         </button>
       </div>
     </div>
